c280cafda7dfd3640ec997b895a9377effa81829
AndersonZamora <anderson_coral@outlook.com>
2024-10-28 14:25:58 -0600

    update form3

--- src/app/(grass)/punto/grass/registro/ui/GrassRegisterForm.tsx
+++ src/app/(grass)/punto/grass/registro/ui/GrassRegisterForm.tsx
@@ -150,7 +150,7 @@ export const GrassRegisterForm = () => {
                         maxLength: { value: 100, message: 'Maximo 100 letras' }
                     })}
                 />
-                && (<span className="text-red-500 font-mono">{errors.description?.message}</span>)
+                {errors.description && (<span className="text-red-500 font-mono">{errors.description?.message}</span>)}
             </div>
             <div className='flex justify-center mt-5 mb-5'>
                 <button
